bd09aae92a00ac7250841e08f9374224cf18a0df
AviralPrateek <aviralprateekhello@gmail.com>
2024-09-29 04:27:54 +0530

    commit all

--- C_assignments.c
+++ C_assignments.c
@@ -1,3 +1,197 @@
+//ASSIGNMENT 1
+//1. Write a program (WAP) to display "Hello World" on console display.
+// #include<stdio.h>
+// int main(){
+// 	printf("Hello World");
+// 	return 0;
+// }
+
+
+//2. WAP to input an integer and display it.
+// #include<stdio.h>
+// int main(){
+// 	int i;
+// 	printf("Enter your integer to display it.");
+// 	scanf("%d",& i);
+// 	printf("Your integer:- %d \n",i);
+// 	return 0;
+// }
+
+//3. WAP to input a decimal number and display it.
+// #include<stdio.h>
+// int main(){
+// 	float i;
+// 	printf("Enter your decimal to display it");
+// 	scanf("%f",& i);
+// 	printf("Your decimal:- %.2f \n ",i);      //.2 demonstrates 2 decimal places.
+// 	return 0;
+// }
+
+//4.	WAP to input a character and display it.
+// #include<stdio.h>
+// int main(){
+// 	char ch;
+// 	printf("Enter your character to be displayed.");
+// 	scanf("%c",& ch);
+// 	printf("Your character is:- %c",ch);
+// 	return 0;
+// }
+
+//5.WAP to input a fraction (rational number) and display.
+//(Ask the user to input numerator and denominator, then display it in the form of p/q without simplification).
+
+// #include<stdio.h>
+// int main(){
+// 	int n,d;
+// 	printf("Enter numerator:-");
+// 	scanf("%d",&n);
+// 	printf("Enter denominator:-");
+// 	scanf("%d",&d);
+
+// 	printf("Your number in p/q form is:- %d/%d",n,d);                  // NOTE
+// 	return 0;
+// }
+
+//6.WAP to add, subtract, multiply, divide numbers.
+
+// #include<stdio.h>
+// int main(){
+// 	float a,b;
+// 	printf("Enter first number:- ");
+// 	scanf("%f",&a);
+// 	printf("Enter second number:-");
+// 	scanf("%f",&b);
+    
+// 	printf("Addition:%f + %f= %f\n",a,b,a+b);             //NOTE
+// 	printf("Subtraction:%f - %f= %f\n",a,b,a-b);
+// 	printf("Multiplication:%f * %f= %f\n",a,b,a*b);
+// 	if(b==0){
+// 		printf("Division not possible with 0 denominator !");
+// 	}
+// 	else{
+// 		printf("Division:%f / %f= %f\n",a,b,a/b);
+// 	}
+	
+// 	return 0;
+// }
+
+
+
+//7.WAP to find Nth root of a number.
+// #include<stdio.h>
+// #include<math.h>
+// int main(){
+// 	double num;
+// 	double n;
+// 	double cal;
+// 	printf("Enter number to calculate its nth root:- ");
+// 	scanf("%lf",&num);
+// 	printf("Enter value of 'n' for nth root calculations:- ");
+// 	scanf("%lf",&n);
+    
+// 	cal= pow(num,1/n);                                               //NOTE: 1/n
+// 	printf("nth root for your number is = %lf",cal);
+// 	return 0;
+// }
+
+
+
+
+//8.WAP for calculation of area of circle.
+
+// #include<stdio.h>
+// #include<math.h>
+// int main(){
+// 	double r, pi=3.14, area;
+
+// 	printf("Enter radius for area calculations:- ");
+// 	scanf("%lf",& r);
+//     area=pi*pow(r,2);
+// 	printf("Area of your circle= %lf square unit.", area);
+// 	return 0;
+// }
+
+
+//9.WAP for SI.
+// #include<stdio.h>
+// int main(){
+// 	double SI,r,p,t;
+// 	printf("Enter Principal amount:- ");
+// 	scanf("%lf",& p);
+// 	printf("Enter Rate:- ");
+// 	scanf("%lf",& r);
+// 	printf("Enter Time:- ");
+// 	scanf("%lf",& t);
+
+// 	SI=(p*r*t)/100;
+// 	printf("Simple interest= %lf", SI);
+	
+// 	return 0;
+
+// }
+
+
+
+//10. WAP to calculate CI.
+//The compound interest formula is:
+
+// A = P (1 + r/n)^(nt)
+
+// Where:
+
+// A = the future value of the investment/loan, including interest
+// P = the principal investment amount (the initial deposit or loan amount)
+// r = the annual interest rate (in decimal form - e.g., 4% = 0.04)
+// n = the number of times that interest is compounded per year
+// t = the time the money is invested or borrowed for, in years
+
+// #include<stdio.h>
+// #include<math.h>
+// int main(){
+// 	double Amount,P,R,T,n,CI;
+// 	printf("Enter Principal amount:- ");
+// 	scanf("%lf",&P);
+// 	printf("Enter Rate of interest:- ");
+// 	scanf("%lf",&R);                        //NOTE: Should enter rate in percent form which'll be converted.
+// 	printf("Enter Time in years:- ");
+// 	scanf("%lf",&T);
+// 	printf("Enter no. of times that interest is compounded per year:- "); 
+// 	scanf("%lf",&n);
+    
+// 	R=R/100;                               //NOTE: Conversion of %rate to formula specific rate.
+// 	Amount = P * pow((1 + R/n),(n*T));
+// 	printf("Amount is:- %lf \n", Amount);
+// 	CI=Amount-P;
+// 	printf("CI is:- %lf \n", CI);
+// 	return 0;
+// }
+
+
+//11.WAP to calculate the gross salary of a person where,
+//   gross salary=basic+da+ta.   da is 10% of basic and ta is 12% of basic.
+
+// #include<stdio.h>
+// int main(){
+// 	double grosssalary,basic,da,ta;
+// 	printf("Enter your basic salary:- ");
+// 	scanf("%lf",& basic);
+// 	da=0.10*basic;
+// 	ta=0.12*basic;
+
+// 	grosssalary= basic+da+ta;
+// 	printf("Your gross salary is:- %lf", grosssalary);
+
+// 	return 0;
+// }
+
+
+
+
+//ASSIGNMENT 2
+
+
+
+
 //1.WAP to practice math functions such as ....
 /*#include<stdio.h>
 #include<math.h>
@@ -92,7 +286,7 @@ int main()
 
 
 
-
+//ASSIGNMENT 3
 
 
 
@@ -294,16 +488,16 @@ int main(){
 
 //WAP to check if a number is a 2 digit number or not.
 
-#include<stdio.h>
-int main(){
-	int num;
-	printf("Enter a number to check if it is a 2 digit no. or not.");
-	scanf("%d",num);
-	if(num>9&&num<100){
-		printf("2 digit number ! %d", num);
-	}
-	else{
-		printf("Not %d", num);
-	}
-	return 0;
-}
+// #include<stdio.h>
+// int main(){
+// 	int num;
+// 	printf("Enter a number to check if it is a 2 digit no. or not.");
+// 	scanf("%d",&num);
+// 	if(num>9&&num<100){
+// 		printf("2 digit number ! %d", num);
+// 	}
+// 	else{
+// 		printf("Not %d", num);
+// 	}
+// 	return 0;
+// }
--- chapter2.c
+++ chapter2.c
@@ -104,14 +104,56 @@ int main(){
 
 //HOMEWORK SET QUESTIONS
 
+
+
+
 //WAP to print avg. of three nos.
+
+// #include <stdio.h>
+// int main(){
+//     float a,b,c,avg;
+//     printf("Enter your 3 nos:-");
+//     scanf("%f %f %f", &a, &b, &c);
+//     avg=((a+b+c)/3);
+//     printf("Your avg. is:- %.2f",avg);
+//     printf("Your 3 numbers are: %f, %f, %f\n",a,b,c);
+//     return 0;
+// }
+
+
+
+//WAP to check if given character is digit or not.
+
+// #include<stdio.h>
+// int main(){
+//     printf("Enter your character to check if it is a digit or not:-");
+//     char ch;
+//     scanf(" %c",&ch);               // note the space before %c to ignore whitespace
+//     if(ch>='0' && ch<='9'){
+//         printf("Digit hai bhai...%c",ch);    
+//     }
+//     else{
+//         printf("Digit nahi hai bhai...%c",ch);
+//     }
+//     return 0;
+// }
+
+//WAP to print the smallest no out of 3 nos.
 
 #include<stdio.h>
 int main(){
     float a,b,c;
-    printf("Enter your 3 nos:-");
-    scanf("%f %f %f", &a, &b, &c);
-    printf("Your avg. is:-%f",((a+b+c)/3));
-    printf("Your 3 numbers are: %f, %f, %f\n",a,b,c);
+    printf("Enter your 3 nos. Press enter after each no.");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a<b&&a<c){
+        printf("a is smallest.");
+    }
+    else if(b<a&&b<c){
+        printf("b is smallest.");
+    }
+    else if(c<a&&c<b){
+        
+    }
     return 0;
 }
+
